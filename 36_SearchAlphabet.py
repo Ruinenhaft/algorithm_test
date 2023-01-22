d852a62677aad671f0ab81df5820c8d2a3899b6e
Ruinenhaft <eulennests@gmail.com>
2023-01-22 12:35:18 +0900

    36th solving

---
+++ 36_SearchAlphabet.py
@@ -0,0 +1,7 @@
+alphabets = input()
+alphabet_list = [i for i in range(97, 123)]
+
+for i in range(len(alphabet_list)):
+    for alphabet in alphabets:
+        if alphabet_list[i] == alphabet:
+            alphabet_list[i] =
